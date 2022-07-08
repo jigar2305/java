6455c973d3cad76b07b56c0cb74e8bf20720f247
@jigar2305 <jigarkp786@gmail.com>
2022-07-08 11:19:57 +0530

    string complate

---
+++ string.java
@@ -0,0 +1,8 @@
+class string{
+	public static void main(String[] args){
+		String s = "jigar";
+		System.out.println(s);
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+	}
+}
